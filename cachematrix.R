ea099172c90d5868f13177bb1f3a8d72de71845c
ArkoC511 <arkoc511@gmail.com>
2021-04-30 02:17:17 +0530

    commit for Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,17 @@
 makeCacheMatrix <- function( m = matrix() ) {
-  i <- NULL ## initializing inverse
-  set <- function( matrix ) {   ## creating set matrix
+  i <- NULL                                           ## initializing inverse
+  set <- function( matrix ) {                         ## creating set matrix
     m <<- matrix
     i <<- NULL
   }
-  get <- function() {  ##getting the matrix
-    m                  ##returning matrix
+  get <- function() {                                 ##getting the matrix
+    m                                                 ##returning matrix
   }
-  setInverse <- function(inverse) {  ## Method to make inverse matrix
+  setInverse <- function(inverse) {                   ## Method to make inverse matrix
     i <<- inverse
   }
-  getInverse <- function() {         ## Method to get inverse matrix
-    i                                ## Return the inverse
+  getInverse <- function() {                          ## Method to get inverse matrix
+    i                                                 ## Return the inverse
   }
   list(set = set, get = get,
        setInverse = setInverse,
@@ -19,13 +19,13 @@ makeCacheMatrix <- function( m = matrix() ) {
 }
 
 cacheSolve <- function(x, ...) {
-  m <- x$getInverse() ## Return the inverse of 'x'
-  if( !is.null(m) ) {   ## Return inverse if its already set
+  m <- x$getInverse()                                 ## Return the inverse of 'x'
+  if( !is.null(m) ) {                                 ## Return inverse if its already set
     message("getting cached data")
     return(m)
   }
-  data <- x$get() ## Get the matrix
-  m <- solve(data) %*% data ## Calculate the inverse
-  x$setInverse(m) ## Set the inverse
-  m ## Return the matrix
+  data <- x$get()                                     ## Get the matrix
+  m <- solve(data) %*% data                           ## Calculate the inverse
+  x$setInverse(m)                                     ## Set the inverse
+  m                                                   ## Return the matrix
 }
